48a0544993ef5c34d36afcfae0a0b14785bfca8e
Jack <58417794+jackluxm@users.noreply.github.com>
2021-07-02 21:32:57 +0800

    debug test 1

--- deploy.sh
+++ deploy.sh
@@ -96,6 +96,7 @@ $CF push
 
 if [ $? -ne 0 ]; then
     echo "${BLUE}print cf push error${END}"
-    $CF logs $IBM_APP_NAME --recent
+    #$CF logs $IBM_APP_NAME --recent
+    $CF logs $IBM_APP_NAME
     exit 1
 fi
